35282d0f71cef6c08d9fc75f376d6ac5f2ab5eca
Gauthier Billot <gauthier.billot@fishingcactus.com>
2016-04-24 20:36:31 +0200

    Clean

--- src/application/application.cpp
+++ src/application/application.cpp
@@ -112,7 +112,6 @@ void App::start()
     #ifdef CEF
         gui::System::getInstance().getHandler().init();
         gui::System::getInstance().loadFile(gengine::application::get().getGuiFilename().CString());
-        puts("loadfile");
     #endif
 }
 
