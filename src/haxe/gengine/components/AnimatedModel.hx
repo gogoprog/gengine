c74a853d829230375bc8c27bbd6a8a1e38a7803f
Gauthier Billot <gauthier.billot@fishingcactus.com>
2016-05-17 20:52:41 +0200

    Updated AnimatedModel.hx

--- src/haxe/gengine/components/AnimatedModel.hx
+++ src/haxe/gengine/components/AnimatedModel.hx
@@ -15,26 +15,26 @@ class AnimatedModel extends UrhoComponent
 
     public inline function setModel(model:Dynamic, ?createBones = true)
     {
-        untyped __js__("this.object.setModel(model, createBones)");
+        this.object.setModel(model, createBones);
     }
 
     public inline function setMaterial(material:Dynamic)
     {
-        untyped __js__("this.object.setMaterial(material)");
+        this.object.setMaterial(material);
     }
 
     public inline function addAnimationState(animation:Dynamic):AnimationState
     {
-        return untyped __js__("this.object.addAnimationState(animation)");
+        return this.object.addAnimationState(animation);
     }
 
     public inline function getAnimationStateByIndex(index:Int):AnimationState
     {
-        return untyped __js__("this.object.getAnimationStateByIndex(index)");
+        return this.object.getAnimationStateByIndex(index);
     }
 
     public inline function setCastShadows(castShadows:Bool)
     {
-        untyped __js__("this.object.setCastShadows(castShadows)");
+        this.object.setCastShadows(castShadows);
     }
 }
