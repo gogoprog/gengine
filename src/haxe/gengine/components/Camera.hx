f82089782bc38bd3217d834a4679dd8cf047cf93
Gauthier Billot <gauthier.billot@fishingcactus.com>
2016-05-19 12:17:10 +0200

    Updated Camera.hx

--- src/haxe/gengine/components/Camera.hx
+++ src/haxe/gengine/components/Camera.hx
@@ -18,51 +18,51 @@ class Camera extends UrhoComponent
 
     public function setOrthoSize(size:Vector2)
     {
-        untyped __js__("this.object.setOrthoSize(size)");
+        this.object.setOrthoSize(size);
     }
 
     public function setOrthographic(orthographic:Bool)
     {
-        untyped __js__("this.object.setOrthographic(orthographic)");
+        this.object.setOrthographic(orthographic);
     }
 
     public function setNearClip(distance:Float)
     {
-        untyped __js__("this.object.setNearClip(distance)");
+        this.object.setNearClip(distance);
     }
 
     public function setFarClip(distance:Float)
     {
-        untyped __js__("this.object.setFarClip(distance)");
+        this.object.setFarClip(distance);
     }
 
     public function setFov(fov:Float)
     {
-        untyped __js__("this.object.setFov(fov)");
+        this.object.setFov(fov);
     }
 
     public function setAspectRatio(ratio:Float)
     {
-        untyped __js__("this.object.setAspectRatio(ratio)");
+        this.object.setAspectRatio(ratio);
     }
 
     public function setAutoAspectRatio(autoAspectRatio:Bool)
     {
-        untyped __js__("this.object.setAutoAspectRatio(autoAspectRatio)");
+        this.object.setAutoAspectRatio(autoAspectRatio);
     }
 
     public function setZoom(zoom:Float)
     {
-        untyped __js__("this.object.setZoom(zoom)");
+        this.object.setZoom(zoom);
     }
 
     public function worldToScreenPoint(worldPoint:Vector3):Vector2
     {
-        return untyped __js__("this.object.worldToScreenPoint(worldPoint)");
+        return this.object.worldToScreenPoint(worldPoint);
     }
 
     public function screenToWorldPoint(screenPoint:Vector3):Vector3
     {
-        return untyped __js__("this.object.screenToWorldPoint(screenPoint)");
+        return this.object.screenToWorldPoint(screenPoint);
     }
 }
