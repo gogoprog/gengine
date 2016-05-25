a5c90dac429a701da8ecfd61e00c3e89607f94e5
Gauthier Billot <gauthier.billot@fishingcactus.com>
2016-05-25 15:39:34 +0200

    Updated Entity.hx

--- src/haxe/gengine/Entity.hx
+++ src/haxe/gengine/Entity.hx
@@ -50,105 +50,105 @@ class Entity extends ash.core.Entity
 
     private function set_position(position:Vector3):Vector3
     {
-        untyped __js__("this.node.setPosition(position)");
+        this.node.setPosition(position);
 
         return position;
     }
 
     private function get_position():Vector3
     {
-        return untyped __js__("this.node.getPosition()");
+        return this.node.getPosition();
     }
 
     private inline function set_scale(scale:Vector3):Vector3
     {
-        untyped __js__("this.node.setScale(scale)");
+        this.node.setScale(scale);
 
         return scale;
     }
 
     private inline function get_scale():Vector3
     {
-        return untyped __js__("this.node.getScale()");
+        return this.node.getScale();
     }
 
     public function setPosition(position:Vector3):Vector3
     {
-        untyped __js__("this.node.setPosition(position)");
+        this.node.setPosition(position);
 
         return position;
     }
 
     public inline function getPosition():Vector3
     {
-        return untyped __js__("this.node.getPosition()");
+        return this.node.getPosition();
     }
 
     public inline function setScale(scale:Vector3):Vector3
     {
-        untyped __js__("this.node.setScale(scale)");
+        this.node.setScale(scale);
 
         return scale;
     }
 
     public inline function getScale():Vector3
     {
-        return untyped __js__("this.node.getScale()");
+        return this.node.getScale();
     }
 
     public inline function setRotation2D(angle:Float)
     {
-        untyped __js__("this.node.setRotation2D(angle)");
+        this.node.setRotation2D(angle);
     }
 
     public inline function getRotation2D():Float
     {
-        return untyped __js__("this.node.getRotation2D()");
+        return this.node.getRotation2D();
     }
 
     public inline function setDirection(direction:Vector3)
     {
-        untyped __js__("this.node.setDirection(direction)");
+        this.node.setDirection(direction);
     }
 
     public inline function getDirection():Vector3
     {
-        return untyped __js__("this.node.getDirection()");
+        return this.node.getDirection();
     }
 
     public inline function getUp():Vector3
     {
-        return untyped __js__("this.node.getUp()");
+        return this.node.getUp();
     }
 
     public inline function getRight():Vector3
     {
-        return untyped __js__("this.node.getRight()");
+        return this.node.getRight();
     }
 
     public inline function setWorldPosition(position:Vector3)
     {
-        untyped __js__("this.node.setWorldPosition(position)");
+        this.node.setWorldPosition(position);
     }
 
     public inline function getWorldPosition():Vector3
     {
-        return untyped __js__("this.node.getWorldPosition()");
+        return this.node.getWorldPosition();
     }
 
     public inline function setWorldScale(scale:Vector3)
     {
-        untyped __js__("this.node.setWorldScale(scale)");
+        this.node.setWorldScale(scale);
     }
 
     public inline function getWorldScale():Vector3
     {
-        return untyped __js__("this.node.getWorldScale()");
+        return this.node.getWorldScale();
     }
 
     public inline function setParent(parent:Entity)
     {
-        untyped __js__("this.node.setParent(parent.node)");
+        this.node.setParent(parent.node);
         internalParent = parent;
     }
 
@@ -168,23 +168,28 @@ class Entity extends ash.core.Entity
         return internalParent;
     }
 
-    public function roll(angle:Float, ?transformSpace:Int = 0)
+    public inline function roll(angle:Float, ?transformSpace:Int = 0)
     {
-        untyped __js__("this.node.roll(angle, transformSpace)");
+        this.node.roll(angle, transformSpace);
     }
 
-    public function yaw(angle:Float, ?transformSpace:Int = 0)
+    public inline function yaw(angle:Float, ?transformSpace:Int = 0)
     {
-        untyped __js__("this.node.yaw(angle, transformSpace)");
+        this.node.yaw(angle, transformSpace);
     }
 
-    public function pitch(angle:Float, ?transformSpace:Int = 0)
+    public inline function pitch(angle:Float, ?transformSpace:Int = 0)
     {
-        untyped __js__("this.node.pitch(angle, transformSpace)");
+        this.node.pitch(angle, transformSpace);
     }
 
-    public function lookAt(position:Vector3, ?upVector = null, ?transformSpace:Int = 2)
+    public inline function lookAt(position:Vector3, ?upVector = null, ?transformSpace:Int = 2)
     {
-        untyped __js__("this.node.lookAt(position, upVector || Module.Vector3(0, 1, 0), transformSpace)");
+        if(upVector == null)
+        {
+            upVector = new Vector3(0, 1, 0);
+        }
+
+        this.node.lookAt(position, upVector, transformSpace);
     }
 }
