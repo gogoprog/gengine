5471f20d7d0fff0edfaa0e9c73401f86af57c283
Gauthier Billot <gauthier.billot@fishingcactus.com>
2016-07-20 22:21:25 +0200

    Updated scriptability

--- src/script/script_urho2d.cpp
+++ src/script/script_urho2d.cpp
@@ -10,6 +10,7 @@
 #include <Urho3D/Urho2D/RigidBody2D.h>
 #include <Urho3D/Urho2D/CollisionBox2D.h>
 #include <Urho3D/Urho2D/CollisionCircle2D.h>
+#include <Urho3D/Graphics/Texture2D.h>
 #include <Urho3D/Core/Context.h>
 
 using namespace Urho3D;
@@ -20,10 +21,17 @@ EMBINDCEFV8_DECLARE_CLASS(Component);
 EMBINDCEFV8_DECLARE_ENUM(LoopMode2D);
 EMBINDCEFV8_DECLARE_ENUM(BlendMode);
 EMBINDCEFV8_DECLARE_ENUM(BodyType2D);
+EMBINDCEFV8_DECLARE_ENUM(TextureAddressMode);
+EMBINDCEFV8_DECLARE_ENUM(TextureCoordinate);
 
 EMBINDCEFV8_BINDINGS(urho2d)
 {
+    embindcefv8::Class<Texture2D>("Texture2D")
+        .method("setAddressMode", static_cast<void (Texture2D::*)(TextureCoordinate, TextureAddressMode)>(&Texture2D::SetAddressMode))
+        ;
+
     embindcefv8::Class<Sprite2D>("Sprite2D")
+        .method("getTexture", &Sprite2D::GetTexture)
         ;
 
     embindcefv8::Class<AnimationSet2D>("AnimationSet2D")
