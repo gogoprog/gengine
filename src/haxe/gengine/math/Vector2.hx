737d1d73f4def266a7f7f3239dee14d116e623a5
Gauthier Billot <gauthier.billot@fishingcactus.com>
2016-10-03 17:28:26 +0200

    Added some operators on Vector classes

--- src/haxe/gengine/math/Vector2.hx
+++ src/haxe/gengine/math/Vector2.hx
@@ -1,11 +1,45 @@
 package gengine.math;
 
 @:native('Module.Vector2')
-extern class Vector2
+extern class Vector2Base
 {
     public var x:Float;
     public var y:Float;
 
     @:selfCall
     public function new(x:Float, y:Float);
+}
+
+@:forward
+abstract Vector2(Vector2Base) to Vector2Base from Vector2Base
+{
+    @:selfCall
+    inline public function new(x:Float, y:Float)
+    {
+        this = new Vector2Base(x, y);
+    }
+
+    @:op(A * B)
+    inline static public function mul(a:Vector2, b:Float)
+    {
+        return new Vector2(a.x * b, a.y * b);
+    }
+
+    @:op(A / B)
+    inline static public function div(a:Vector2, b:Float)
+    {
+        return new Vector2(a.x / b, a.y / b);
+    }
+
+    @:op(A + B)
+    inline static public function add(a:Vector2, b:Vector2)
+    {
+        return new Vector2(a.x + b.x, a.y + b.y);
+    }
+
+    @:op(A - B)
+    inline static public function min(a:Vector2, b:Vector2)
+    {
+        return new Vector2(a.x - b.x, a.y - b.y);
+    }
 }
--- src/haxe/gengine/math/Vector3.hx
+++ src/haxe/gengine/math/Vector3.hx
@@ -1,7 +1,7 @@
 package gengine.math;
 
 @:native('Module.Vector3')
-extern class Vector3
+extern class Vector3Base
 {
     public var x:Float;
     public var y:Float;
@@ -9,4 +9,39 @@ extern class Vector3
 
     @:selfCall
     public function new(x:Float, y:Float, z:Float);
+}
+
+
+@:forward
+abstract Vector3(Vector3Base) to Vector3Base from Vector3Base
+{
+    @:selfCall
+    inline public function new(x:Float, y:Float, z:Float)
+    {
+        this = new Vector3Base(x, y, z);
+    }
+
+    @:op(A * B)
+    inline static public function mul(a:Vector3, b:Float)
+    {
+        return new Vector3(a.x * b, a.y * b, a.z * b);
+    }
+
+    @:op(A / B)
+    inline static public function div(a:Vector3, b:Float)
+    {
+        return new Vector3(a.x / b, a.y / b, a.z / b);
+    }
+
+    @:op(A + B)
+    inline static public function add(a:Vector3, b:Vector3)
+    {
+        return new Vector3(a.x + b.x, a.y + b.y, a.z + b.z);
+    }
+
+    @:op(A - B)
+    inline static public function min(a:Vector3, b:Vector3)
+    {
+        return new Vector3(a.x - b.x, a.y - b.y, a.z - b.z);
+    }
 }
