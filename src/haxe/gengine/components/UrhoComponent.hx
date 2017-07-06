7eebe60780a4cc083fae4a0b82477fb9eb7dc967
Gauthier Billot <gauthier.billot@fishingcactus.com>
2017-07-06 13:04:33 +0200

    Functional generation

--- src/haxe/gengine/components/UrhoComponent.hx
+++ src/haxe/gengine/components/UrhoComponent.hx
@@ -1,13 +1,3 @@
 package gengine.components;
 
-import gengine.math.*;
-
-@:dox(hide)
-class UrhoComponent
-{
-    public var object:Dynamic;
-
-    public function new()
-    {
-    }
-}
+typedef UrhoComponent = Component;
